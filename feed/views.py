f494c1508fc11e5efc0c6bfaaed73e6d2fea7b35
Werner Marx <werner.marx@outlook.com>
2023-07-30 19:22:38 -0600

    workstation update

--- feed/views.py
+++ feed/views.py
@@ -1,10 +1,11 @@
 from typing import Any, Dict
 from django.views.generic import TemplateView
+from .models import Post
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['my_thing'] = "Hello world"
+        context['posts'] = Post.objects.all()
         return context
