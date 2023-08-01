94d2d1aab043a1afaa07d02fc1091ef9839c08b5
Werner Marx <werner.marx@outlook.com>
2023-07-31 18:06:38 -0600

    moving to laptop

--- feed/views.py
+++ feed/views.py
@@ -1,5 +1,5 @@
 from typing import Any, Dict
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 from .models import Post
 
 class HomePageView(TemplateView):
@@ -9,3 +9,7 @@ class HomePageView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['posts'] = Post.objects.all()
         return context
+    
+class PostDetailView(DetailView):
+    template_name = "detail.html"
+    model = Post
